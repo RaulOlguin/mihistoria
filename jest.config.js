06c1d950ab4a0b6ee7bcf238813bcd699f1115fd
Cristopher <cr.ossesg@duocuc.cl>
2025-10-23 11:41:53 -0300

    Testing V0.4 NavBar componentes - no funcional

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
+    "^lucide-react$": "<rootDir>/test/mocks/lucide-react.js",
     "^@/(.*)$": "<rootDir>/$1",
     "^app/(.*)$": "<rootDir>/app/$1",
   },
---
+++ test/mocks/lucide-react.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Home: (props) => <svg {...props} data-testid="icon-home" />,
+  Image: (props) => <svg {...props} data-testid="icon-image" />,
+  NotebookPen: (props) => <svg {...props} data-testid="icon-notebook" />,
+  CircleQuestionMark: (props) => <svg {...props} data-testid="icon-question" />,
+};
